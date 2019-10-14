584560cc5bb5b3634d914e4b32e6b3865f36f791
Zim Kalinowski <zikalino@microsoft.com>
2019-10-14 13:16:21 +0800

    manual fix

--- src/peering/azext_peering/action.py
+++ src/peering/azext_peering/action.py
@@ -28,7 +28,7 @@ class PeeringAddDirectConnection(argparse._AppendAction):
         for k in properties:
             kl = k.lower()
             v = properties[k]
-            if kl == 'bandwithdinmbps':
+            if kl == 'bandwidthdinmbps':
                 d['bandwidthInMbps'] = v
             elif kl == 'sessionaddressprovider':
                 d['sessionAddressProvider'] = v
