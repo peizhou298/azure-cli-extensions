cfea8a4d57b8722f5309c5a5bacfb6981504a324
Zim Kalinowski <zikalino@microsoft.com>
2019-10-14 13:04:09 +0800

    additional stuff

--- src/peering/azext_peering/action.py
+++ src/peering/azext_peering/action.py
@@ -20,11 +20,37 @@ class PeeringAddDirectConnection(argparse._AppendAction):
         super(PeeringAddDirectConnection, self).__call__(parser, namespace, action, option_string)
 
     def get_action(self, values, option_string):  # pylint: disable=no-self-use
-        #_type = values[0].lower()
-
-        # iterate through values
-        return {}
-        #raise CLIError('usage error: {} TYPE KEY [ARGS]'.format(option_string))
+        try:
+            properties = dict(x.split('=', 1) for x in values)
+        except ValueError:
+            raise CLIError('usage error: {} [KEY=VALUE ...]'.format(option_string))
+        d = {}
+        for x in properties:
+            k = x[0].lower()
+            v = x[1]
+            if k == 'bandwithdinmbps':
+                d['bandwidthInMbps'] = v
+            elif k == 'sessionaddressprovider':
+                d['sessionAddressProvider'] = v
+            elif k == 'useforpeeringservice':
+                d['useForPeeringService'] = v
+            elif k == 'peeringdbfacilityid':
+                d['peeringDBFacilityId'] = v
+            elif k == 'sessionprefixv4':
+                d.setdefault('bgpSession', {})['sessionPrefixV4'] = v
+            elif k == 'sessionprefixv6':
+                d.setdefault('bgpSession', {})['sessionPrefixV6'] = v
+            elif k == 'maxprefixesadvertisedv4':
+                d.setdefault('bgpSession', {})['maxPrefixesAdvertisedV4'] = v
+            elif k == 'maxprefixesadvertisedv6':
+                d.setdefault('bgpSession', {})['maxPrefixesAdvertisedV6'] = v
+            elif k == 'md5authenticationkey':
+                d.setdefault('bgpSession', {})['md5AuthenticationKey'] = v
+            elif k == 'connectionidentifier':
+                d['connectionIdentifier'] = v
+            else:
+                raise CLIError('usage error: {} is invalid'.format(k))
+        return d
 
 
 # pylint: disable=protected-access
